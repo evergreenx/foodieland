01b80d639ce41f245c95d34780213c52f82f603f
ido dickson evergreen <dicksonevergreenido@gmail.com>
2022-07-21 15:46:19 +0100

    fix: add setup files

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,25 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ChakraProvider } from '@chakra-ui/react'
+import { extendTheme } from '@chakra-ui/react'
 
+const colors = {
+  brand: {
+    900: '#1a365d',
+    800: '#153e75',
+    700: '#2a69ac',
+  },
+}
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const theme = extendTheme({ colors })
+
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
+  
+  
 }
 
 export default MyApp
